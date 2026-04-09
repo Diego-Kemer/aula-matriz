f85d8392507a5ab6f9091832b848f9de8a915038
Diego <diegokemer26gmail.com>
2026-04-09 10:02:44 -0300

    feat: add reactive lesson form with validation test

--- src/app/features/lesson-form/pages/lesson-form-page/lesson-form-page.spec.ts
+++ src/app/features/lesson-form/pages/lesson-form-page/lesson-form-page.spec.ts
@@ -3,21 +3,38 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LessonFormPage } from './lesson-form-page';
 
 describe('LessonFormPage', () => {
-  let component: LessonFormPage;
   let fixture: ComponentFixture<LessonFormPage>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LessonFormPage]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LessonFormPage);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should keep submit button disabled when form is invalid', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button') as HTMLButtonElement;
+
+    expect(button.disabled).toBe(true);
   });
-});
+
+  it('should enable submit button when form is valid', async () => {
+    const component = fixture.componentInstance;
+    component.lessonForm.patchValue({
+      title: 'El cuento fantástico',
+      subject: 'Lengua y Literatura',
+      course: '3er año'
+    })
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button') as HTMLButtonElement;
+
+    expect(button.disabled).toBe(false);
+  })
+})
--- src/app/features/lesson-form/pages/lesson-form-page/lesson-form-page.ts
+++ src/app/features/lesson-form/pages/lesson-form-page/lesson-form-page.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lesson-form-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './lesson-form-page.html',
   styleUrl: './lesson-form-page.css',
 })
-export class LessonFormPage {
+export class LessonFormPage implements OnInit {
+  private fb = inject(FormBuilder)
+  lessonForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.lessonForm = this.fb.group({
+      title: ['', [Validators.required]],
+      subject: ['', [Validators.required]],
+      course: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit(){
+    console.log(this.lessonForm.value)
+  }
 }
